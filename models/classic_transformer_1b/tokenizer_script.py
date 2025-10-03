d9da9996a290149610185526afa25a2ddcf67ef2
codespaceDrifter <codeCraftCorpo@gmail.com>
2025-10-03 04:01:49 -0700

    drift

--- models/classic_transformer_1b/tokenizer_script.py
+++ models/classic_transformer_1b/tokenizer_script.py
@@ -3,7 +3,7 @@ from utils.tokenizer.tokenizer import *
 
 # python3 -m models.classic_transformer_1b.tokenizer_script
 
-tokenizer = Tokenizer(vocab_size=1000)
+tokenizer = Tokenizer()
 
 tokenizer._fit_vocab_source(txt_path='models/classic_transformer_1b/tokenizer/data/mixed_vocab_data.txt')
 
